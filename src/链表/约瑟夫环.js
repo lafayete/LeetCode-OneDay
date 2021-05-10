bb8bb018a40806451454303044453664dc096f58
wushibao <wushibao2008@qq.com>
2021-05-10 22:43:23 +0800

    feat: add last remaining

---
+++ src/链表/约瑟夫环.js
@@ -0,0 +1,39 @@
+function LastRemaining_Solution(n, m) {
+    if( n < 1 || m < 1) {
+
+        return -1;
+    }
+    let head = {
+        val: 0
+    }
+
+    let current = head;
+    
+    for(let i=1; i < n;i++) {
+        current.next = {
+            val: i
+        };
+        current = current.next;
+    }
+
+    current.next = head;
+
+    while (current.next !==current) {
+
+        for(let i =0; i< m; i++) {
+
+            current = current.next;
+
+        }
+
+        current.next = current.next.next;
+
+
+    }
+
+    return current.val;
+
+
+
+
+}
