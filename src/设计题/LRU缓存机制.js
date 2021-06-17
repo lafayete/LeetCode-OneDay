03d7b5d73270271302b30f9ca8d1b289cd2bc73c
wushibao <wushibao@chengfayun.com>
2021-06-17 11:31:28 +0800

    d

--- src/设计题/LRU缓存机制.js
+++ src/设计题/LRU缓存机制.js
@@ -1,39 +1,51 @@
 // LRU的缓存机制
+
+// 常规思路 哈希表 + 双向链表
+// 利用JS的map属性
 var LRUCache = function(capacity) {
 
     this.map = new Map();
 
     this.maxSize = capacity;
-
-    this.queue = [];
 };
 
 LRUCache.prototype.get = function(key) {
 
+    if(!this.map.has(key)) return -1;
 
-    let obj = this.map.get(key);
+    let tmp = this.map.get(key);
+    
+    this.map.delete(key);
 
-    this.queue.push(key);
+    this.map.set(key, tmp);
 
-    return obj.value || -1;
-
+    return tmp;
 };
 
 
 LRUCache.prototype.put = function(key, value) {
 
+    if(this.map.has(key)) {
+        this.map.delete(key);
+    }
 
-    let size = this.map.size();
+    this.map.set(key,value);
 
-    if(size > this.maxSize) {
+    if(this.map.size > this.maxSize) {
 
-        let lastKey = this.queue.shift();
-
-        this.map.delete(lastKey);
-        
-    } else {
-
-        this.map(key, value);
+        this.map.delete(this.map.keys().next().value);
     }
 
 };
+
+
+let lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // 缓存是 {1=1}
+lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+lRUCache.get(1);    // 返回 1
+lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+lRUCache.get(2);    // 返回 -1 (未找到)
+lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+lRUCache.get(1);    // 返回 -1 (未找到)
+lRUCache.get(3);    // 返回 3
+lRUCache.get(4);    // 返回 4
