1cd8b37d47f0cb5d4f82d244361305f6b5695a24
wushibao <wushibao@chengfayun.com>
2021-06-19 13:39:03 +0800

    feat: add

---
+++ src/链表/合并两个有序链表.js
@@ -0,0 +1,62 @@
+// 递归
+
+var mergeTwoLists = function(l1,l2) {
+
+    if(l1 == null) {
+
+        return l2;
+
+    } else if( l2 == null) {
+
+        return l1;
+
+    } else {
+
+        if(l1.val < l2.val) {
+
+            l1.next = mergeTwoLists(l1.next, l2);
+
+            return l1;
+
+        } else {
+
+            l2.next = mergeTwoLists(l2.next, l1);
+
+            // return l2;/
+        }
+
+    }
+
+}
+
+// 迭代法
+var mergeTwoLists = function(l1, l2) {
+
+    const prevhead = new ListNode(-1);
+
+    let prev = prevhead;
+
+    while(l1!==null || l2!=null) {
+
+        if(l1.val<=l2.val) {
+
+            prev.next = l1;
+
+            l1 = l1.next;
+
+        } else {
+
+            prev.next = l2;
+
+            l2 = l2.next;
+        }
+
+        prev = prev.next;
+
+    }
+
+    prev.next = l1 !== null? l1 : l2;
+
+    return prev;
+
+};
---
+++ src/链表/相交链表.js
@@ -0,0 +1,17 @@
+var getIntersectionNode = function(headA, headB) {
+    
+    let a = headA;
+
+    let b = headB;
+
+    while(a !== b) {
+
+        a =  a == null ? headB : a.next;
+
+        b = b == null ? headA: b.next
+
+    }
+
+    return a;
+
+};
