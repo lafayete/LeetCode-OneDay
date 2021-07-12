b39f5eae9a919231b608f1599cad770fc536319b
wushibao <wushibao2008@qq.com>
2021-07-12 20:58:57 +0800

    f

---
+++ src/链表/回文链表.js
@@ -0,0 +1,43 @@
+var isPalindrome = function(head) {
+
+
+    if(head === null) {
+        return false;
+    }
+
+    let stack = [];
+
+    while(head !==null) {
+
+        stack.push(head.val);
+
+        head = head.next;
+
+    }
+
+
+
+    let left = 0;
+
+    let right = stack.length - 1;
+
+    while(left <= right) {
+
+
+        if(stack[left] === stack[right]) {
+
+            left++;
+
+            right--;
+
+        } else {
+
+            return false;
+
+        }
+
+    }
+
+    return true;
+
+};
